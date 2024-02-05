b9d234e3b908a81cb9060010c0a3e7aae7dd22a6
Alexandre XYZ <costaalexandre45@gmail.com>
2024-02-05 02:05:54 -0300

    Fix editor template

--- components/HtmlEditor.tsx
+++ components/HtmlEditor.tsx
@@ -9,25 +9,31 @@ import 'ace-builds/src-noconflict/theme-monokai';
 export default function HtmlEditor({ value, onChange }: any) {
 
     return (
-        <div className='flex gap-4 justify-center w-full' >
-            <AceEditor
-                mode="html"
-                theme="light"
-                value={value}
-                onChange={onChange}
-                name="html_editor"
-                editorProps={{ $blockScrolling: true }}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: false,
-                    showLineNumbers: true,
-                    tabSize: 4,
-                }}
-            />
-            <div className='w-1/2 border-black border'>
+        <div className='flex gap-4 justify-center w-full flex-col' >
+            <div className='h-[200px] resize-y overflow-auto pb-2'>
+                <AceEditor
+                    mode="html"
+                    theme="Amy"
+                    width='100%'
+                    height='100%'
+                    value={value}
+                    onChange={onChange}
+                    name="html_editor"
+                    editorProps={{ $blockScrolling: true }}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                        tabSize: 4,
+                    }}
+                />
+            </div>
+            <div>
                 <h2>Preview</h2>
-                <div dangerouslySetInnerHTML={{ __html: `${value}` }} />
+                <div className='border-black border'>
+                    <div dangerouslySetInnerHTML={{ __html: `${value}` }} />
+                </div>
             </div>
         </div>
     );

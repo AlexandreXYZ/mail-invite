dbf4e9b0d2be5e75ee0ec11bcce1c768dac05a14
Alexandre XYZ <costaalexandre45@gmail.com>
2024-02-13 00:26:54 -0300

    Fix tailwind css normalize

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,11 @@ export default function Home() {
   };
 
   return (
-    <div className="w-[1000px] mx-auto pt-4">
-      <input className="w-full p-2 mb-2 outline-blue-500 focus:outline focus:outline-4 border border-black" type='text' placeholder='Titulo' value={subject} onChange={(e) => setSubject(e.target.value)} />
-      <input className="w-full p-2 mb-2 outline-blue-500 focus:outline focus:outline-4 border border-black" type='email' placeholder='Exemplo@exemplo.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+    <div className="pt-4">
+      <div className="w-[400px]">
+        <input className="w-full p-2 mb-2 outline-blue-500 focus:outline focus:outline-4 border border-black" type='text' placeholder='Titulo' value={subject} onChange={(e) => setSubject(e.target.value)} />
+        <input className="w-full p-2 mb-2 outline-blue-500 focus:outline focus:outline-4 border border-black" type='email' placeholder='Exemplo@exemplo.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
       <HtmlEditor value={content} onChange={(e: any) => setContent(e)} />
       <button className="mt-4 mx-auto block bg-[blue] w-[200px] text-white rounded-md text-primary p-2 outline-blue-500 focus:outline focus:outline-4 font-medium active:bg-blue-500 active:text-white" onClick={onSend}>Enviar</button>
     </div>
